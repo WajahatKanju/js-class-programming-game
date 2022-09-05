69346f1f675253e703016b868da5fb58252f8409
WjahatAhmad <wajahat5ahmad@gmail.com>
2022-09-06 00:06:22 +0500

    added spiders

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 /** @type {HTMLCanvasElement} */
 
-
 window.addEventListener("load", () => {
   const ctx = canvas.getContext("2d");
   const CANVAS_WIDTH = (ctx.canvas.width = innerWidth);
@@ -8,13 +7,12 @@ window.addEventListener("load", () => {
   let previousTime = 0;
   let interval = 0;
 
-  function generateRandomBetween(min , max){
+  function generateRandomBetween(min, max) {
     return Math.random() * (max - min) + min;
   }
-  
 
-  class Game{
-    constructor(ctx){
+  class Game {
+    constructor(ctx) {
       this.ctx = ctx;
       this.width = this.ctx.canvas.width;
       this.height = this.ctx.canvas.height;
@@ -22,25 +20,24 @@ window.addEventListener("load", () => {
       this.timeToEnemy = 0;
       this.enemys = [];
     }
-    draw(){
+    draw() {
       this.ctx.clearRect(0, 0, this.width, this.height);
-      [...this.enemys].forEach(object => object.draw(this.ctx));
-      [...this.enemys].forEach(object => object.update());
+      [...this.enemys].forEach((object) => object.draw(this.ctx));
+      [...this.enemys].forEach((object) => object.update());
     }
-    update(deltaTime){
-      if(this.timeToEnemy > this.enemyTiming ){
-        this.enemys = this.enemys.filter(enemy => !enemy.markedForDeletion);
+    update(deltaTime) {
+      if (this.timeToEnemy > this.enemyTiming) {
+        this.enemys = this.enemys.filter((enemy) => !enemy.markedForDeletion);
         this.timeToEnemy = 0;
         this.#addEnemy();
-        console.log(this.enemys);
-      }else{
+      } else {
         this.timeToEnemy += deltaTime;
       }
       this.draw();
     }
 
-    #addEnemy(){
-      this.enemys.push(new Enemy(this));
+    #addEnemy() {
+      this.enemys.push(new Spider(this));
     }
   }
 
@@ -49,10 +46,10 @@ window.addEventListener("load", () => {
       this.game = game;
       this.width = 100;
       this.height = 100;
-      
+
       this.x = CANVAS_WIDTH;
       this.y = generateRandomBetween(0, this.game.height - this.height);
-      this.dx = Math.random() * 10 + 5;
+      this.dx = generateRandomBetween(10, 15);
       this.markedForDeletion = false;
     }
     draw(ctx) {
@@ -60,7 +57,55 @@ window.addEventListener("load", () => {
     }
     update() {
       this.x -= this.dx;
-      if(this.x + this.width < 0){
+      if (this.x + this.width < 0) {
+        this.markedForDeletion = true;
+      }
+    }
+  }
+
+  class Spider extends Enemy {
+    constructor(game) {
+      super(game);
+      this.image = spider;
+      this.rows = 6;
+      this.cols = 1;
+      this.spriteWidth = this.image.width / this.rows;
+      this.spriteHeight = this.image.height / this.cols;
+      this.sizeModifier = generateRandomBetween(0.3, 0.8);
+      this.maximumHeight = generateRandomBetween(0, game.ctx.canvas.height/2 + 100 );
+      this.width = this.spriteWidth * 0.5;
+      this.height = this.spriteHeight * 0.5;
+      this.frame = 0;
+      this.dx = 0;
+      this.dy = generateRandomBetween(5, 7);
+      this.x = generateRandomBetween(0, game.ctx.canvas.width - this.width);
+      this.y = -this.height;
+    }
+    draw(ctx) {
+      ctx.beginPath();
+      ctx.moveTo(this.x + this.width/2, 0);
+      ctx.lineTo(this.x + this.width/2, this.y);
+      ctx.stroke();
+      ctx.drawImage(
+        this.image,
+        this.spriteWidth * this.frame,
+        0,
+        this.spriteWidth,
+        this.spriteHeight,
+        this.x,
+        this.y,
+        this.width,
+        this.height
+      );
+    }
+    update(){
+      super.update();
+      
+      this.y += this.dy;
+      if(this.y > this.maximumHeight){
+        this.dy = -this.dy;
+      }
+      if(this.y < -this.height*2){
         this.markedForDeletion = true;
       }
     }
