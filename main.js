d0a73ef611eff75dca9b272c5439c97a48eb8747
WjahatAhmad <wajahat5ahmad@gmail.com>
2022-09-05 18:44:27 +0500

    initial Commit

---
+++ main.js
@@ -0,0 +1,9 @@
+/** @type {HTMLCanvasElement} */
+
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = ctx.canvas.width = innerWidth;
+const CANVAS_HEIGHT = ctx.canvas.height = innerHeight;
+
+document.addEventListener('load', ()=> {
+
+})
